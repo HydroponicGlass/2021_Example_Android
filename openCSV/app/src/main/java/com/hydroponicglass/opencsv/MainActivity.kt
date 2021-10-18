92ae4db5cb44cc78b2f77f4b00a04bd4e06b9c3f
HydroponicGlass <hydroponicglass@gmail.com>
2021-10-18 18:42:20 +0900

    openCSV add

---
+++ openCSV/app/src/main/java/com/hydroponicglass/opencsv/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.hydroponicglass.opencsv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView : TextView = findViewById(R.id.textView)
+
+        val csvFile = InputStreamReader(assets.open("data.csv"))
+        val reader = BufferedReader(csvFile)
+
+        var tmpLine : String
+        var output : String = ""
+
+        try
+        {
+            while(reader.readLine().also { tmpLine = it } != null)
+            {
+                val row = tmpLine.split(',')
+                output = output + row[0] + " " + row[1] + '\n'
+                Log.d("debug", "output : " + output)
+            }
+        } catch (e:Exception)
+        {
+            e.printStackTrace()
+        }
+        textView.setText(output)
+    }
+}
