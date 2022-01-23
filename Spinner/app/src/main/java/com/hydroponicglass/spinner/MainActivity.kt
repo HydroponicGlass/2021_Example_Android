1437fd1548d4807f6360f600611a339ff38c7fb0
HydroponicGlass <hydroponicglass@gmail.com>
2022-01-23 16:31:23 +0900

    Add : Spinner

---
+++ Spinner/app/src/main/java/com/hydroponicglass/spinner/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.hydroponicglass.spinner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val spinner: Spinner = findViewById(R.id.spinner)
+        val textView: TextView = findViewById(R.id.textView)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.spinner_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when (position) {
+                    0 -> textView.setText("aaa")
+                    1 -> textView.setText("bbb")
+                    2 -> textView.setText("ccc")
+                    else -> textView.setText("error")
+                }
+            }
+        }
+    }
+}
