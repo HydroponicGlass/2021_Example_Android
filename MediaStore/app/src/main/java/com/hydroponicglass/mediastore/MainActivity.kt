04cb05dcb150b3ff51b599b67c3e3ca50395c48b
HydroponicGlass <hydroponicglass@gmail.com>
2022-04-24 15:11:05 +0900

    add : refer site is added

--- MediaStore/app/src/main/java/com/hydroponicglass/mediastore/MainActivity.kt
+++ MediaStore/app/src/main/java/com/hydroponicglass/mediastore/MainActivity.kt
@@ -1,3 +1,9 @@
+/*  Ref
+https://developer.android.com/training/data-storage/shared/media
+https://dnight.tistory.com/entry/MediaStore-File-%EB%8B%A4%EB%A3%A8%EA%B8%B0-Android-ScopedStorage
+https://codechacha.com/ko/android-mediastore-read-media-files/
+ */
+
 package com.hydroponicglass.mediastore
 
 import android.content.pm.PackageManager
