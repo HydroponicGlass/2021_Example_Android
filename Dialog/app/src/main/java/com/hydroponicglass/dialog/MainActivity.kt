22a9ae53f102defe0a056e0efd02a736023f712d
HydroponicGlass <hydroponicglass@gmail.com>
2022-05-23 20:50:36 +0900

    add : custom editText

--- Dialog/app/src/main/java/com/hydroponicglass/dialog/MainActivity.kt
+++ Dialog/app/src/main/java/com/hydroponicglass/dialog/MainActivity.kt
@@ -5,21 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.EditText
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.marginLeft
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var editText = EditText(this)
+        var dialog_editText = layoutInflater.inflate(R.layout.dialog_edittext, null)
         val builder = AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth)
         builder
             .setTitle("Title")
             .setMessage("MessageMessageMessageMessageMessageMessage")
-            .setView(editText)
+            .setView(dialog_editText)
             .setPositiveButton("Start",
                 DialogInterface.OnClickListener { dialog, id ->
+                    var editText = dialog_editText.findViewById<EditText>(R.id.editText)
                     Log.d("test", editText.text.toString())
                 })
             .setNegativeButton("Cancel",
