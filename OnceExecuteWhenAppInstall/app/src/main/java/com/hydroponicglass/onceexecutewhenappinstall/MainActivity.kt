e06c82609fa02c477c8d9adaeeb90de361531bc6
HydroponicGlass <hydroponicglass@gmail.com>
2022-07-06 20:34:33 +0900

    Fix : Missing files update

--- OnceExecuteWhenAppInstall/app/src/main/java/com/hydroponicglass/onceexecutewhenappinstall/MainActivity.kt
+++ OnceExecuteWhenAppInstall/app/src/main/java/com/hydroponicglass/onceexecutewhenappinstall/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.hydroponicglass.onceexecutewhenappinstall
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pref : SharedPreferences = getSharedPreferences("CheckFirst", Context.MODE_PRIVATE)
+        var checkFirst = pref.getBoolean("CheckFirst", false)
+        if(checkFirst == false){
+            var editer : SharedPreferences.Editor = pref.edit()
+            editer.putBoolean("CheckFirst", true)
+            editer.commit()
+            Log.d("test", "First Execute")
+        }
     }
 }
