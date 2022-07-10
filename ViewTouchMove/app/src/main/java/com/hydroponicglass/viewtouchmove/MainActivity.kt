f2dfe5e1080e19bff2d98d17a86ae07ead6384f0
HydroponicGlass <hydroponicglass@gmail.com>
2022-07-10 09:59:54 +0900

    Add : ViewTouchMove is implemented

--- ViewTouchMove/app/src/main/java/com/hydroponicglass/viewtouchmove/MainActivity.kt
+++ ViewTouchMove/app/src/main/java/com/hydroponicglass/viewtouchmove/MainActivity.kt
@@ -2,10 +2,35 @@ package com.hydroponicglass.viewtouchmove
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        var startX = 0f
+        var startY = 0f
+
+        imageView.setOnTouchListener { v, event ->
+            when (event.action) {
+
+                MotionEvent.ACTION_DOWN -> {
+                    startX = event.x
+                    startY = event.y
+                }
+
+                MotionEvent.ACTION_MOVE -> {
+                    val movedX: Float = event.x - startX
+                    val movedY: Float = event.y - startY
+
+                    v.x = v.x + movedX
+                    v.y = v.y + movedY
+                }
+            }
+            true
+        }
     }
 }
