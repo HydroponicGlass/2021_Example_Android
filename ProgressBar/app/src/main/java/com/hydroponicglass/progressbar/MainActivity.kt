8e4d5bbe8ae7d2836f9f58e6ec212713ae12e572
HydroponicGlass <hydroponicglass@gmail.com>
2022-12-17 19:19:43 +0900

    Add : ProgressBar first commit

---
+++ ProgressBar/app/src/main/java/com/hydroponicglass/progressbar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hydroponicglass.progressbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
