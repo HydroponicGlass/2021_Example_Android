19428a07861a05afd14c146585caaeeb1a915276
HydroponicGlass <hydroponicglass@gmail.com>
2022-12-17 19:44:49 +0900

    Add : Custom Progress bar

--- ProgressBar/app/src/main/java/com/hydroponicglass/progressbar/MainActivity.kt
+++ ProgressBar/app/src/main/java/com/hydroponicglass/progressbar/MainActivity.kt
@@ -2,10 +2,24 @@ package com.hydroponicglass.progressbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val progress_bar: ProgressBar = findViewById(R.id.progress_bar)
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener{
+            val progress = progress_bar.progress
+//            progress_bar.max = 30
+            progress_bar.setProgress(progress+10)
+        }
+
+
     }
 }
