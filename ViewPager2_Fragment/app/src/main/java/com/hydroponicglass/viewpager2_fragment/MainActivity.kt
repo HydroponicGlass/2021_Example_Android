23ab2276fd45cdf9129c12f304133ea3607c7f58
HydroponicGlass <hydroponicglass@gmail.com>
2023-03-01 21:12:47 +0900

    [ViewPager2_Fragment] implement complete

--- ViewPager2_Fragment/app/src/main/java/com/hydroponicglass/viewpager2_fragment/MainActivity.kt
+++ ViewPager2_Fragment/app/src/main/java/com/hydroponicglass/viewpager2_fragment/MainActivity.kt
@@ -2,10 +2,28 @@ package com.hydroponicglass.viewpager2_fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
+import com.hydroponicglass.viewpager2_fragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragmentPagerAdapter = PagerFragmentStateAdapter(this)
+        fragmentPagerAdapter.addFragment(Fragment1())
+        fragmentPagerAdapter.addFragment(Fragment2())
+
+
+        binding.viewPager.adapter = fragmentPagerAdapter
+
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+            tab.text = "Tab ${position+1}"
+        }.attach()
+
+//        binding.viewPager.setUserInputEnabled(false)
     }
 }
